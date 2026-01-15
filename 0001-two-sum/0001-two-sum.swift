774ac23513022026012618a53a753e0c87320818
Bambu <byrr113@gmail.com>
2026-01-15 12:00:35 +0900

    Time: 0 ms (100%), Space: 20 MB (42.69%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.swift
@@ -0,0 +1,30 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        // var result: [Int] = []
+        // for i in 0..<nums.count {
+        //     guard (i + 1) < nums.count else { break }
+        //     for j in (i + 1)..<nums.count {
+        //         if nums[i] + nums[j] == target {
+        //             result = [i, j]
+        //             break
+        //         }
+        //     }
+        //     if !result.isEmpty { break }
+        // }
+        // return result
+
+        var result: [Int] = []
+        var map = [Int: Int]() // [num: idx]
+
+        for i in 0..<nums.count {
+            let other = target - nums[i]
+
+            if let j = map[other] {
+                result = [i, j]
+            }
+
+            map[nums[i]] = i
+        }
+        return result
+    }
+}
