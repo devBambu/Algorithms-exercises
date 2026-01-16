c64e6fc97bddc7ad618b0f924a00b2a333013e81
Bambu <byrr113@gmail.com>
2026-01-16 12:31:04 +0900

    Update 0014-longest-common-prefix.swift

--- LeetCode/0014-longest-common-prefix/0014-longest-common-prefix.swift
+++ LeetCode/0014-longest-common-prefix/0014-longest-common-prefix.swift
@@ -1,8 +1,8 @@
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
         let sortedStrs = strs.sorted() // 제시 배열 오름차순 정렬
-        var prefix = sortedStrs.first!.map { String($0) } // 가장 짧은 단어 할당
-
+        var prefix = sortedStrs.first!.map { String($0) } // 가장 짧은 단어 할당 -- 아래 prefix 비교 반복문에서 인덱스 오류를 일으키지 않기 위해 가장 작은 단어를 할당
+                                                          // + 공통 접두사의 길이 < 주어진 가장 짧은 단어 (항상)
         for s in strs {
             let word = s.map { String($0) } // 비교할 단어
 
