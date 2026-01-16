c9ab0117c957222567952fbb2d71492dd1fe6b87
Bambu <byrr113@gmail.com>
2026-01-16 12:13:30 +0900

    Time: 6 ms (30.61%), Space: 20.3 MB (15.93%) - LeetHub

--- LeetCode/0014-longest-common-prefix/0014-longest-common-prefix.swift
+++ LeetCode/0014-longest-common-prefix/0014-longest-common-prefix.swift
@@ -1,20 +1,18 @@
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
-        let sortedStrs = strs.sorted()
-        var prefix = sortedStrs.first!.map { String($0) }
+        let sortedStrs = strs.sorted() // 제시 배열 오름차순 정렬
+        var prefix = sortedStrs.first!.map { String($0) } // 가장 짧은 단어 할당
 
         for s in strs {
-            let word = s.map { String($0) }
+            let word = s.map { String($0) } // 비교할 단어
 
-            for i in 0..<prefix.count {
-                guard i < word.count else { break }
-                if prefix[i] != word[i] {
-                    prefix = Array(prefix[..<i])
+            for i in 0..<prefix.count { // prefix를 순회하며 비교
+                if prefix[i] != word[i] { // 비교한 두 글자가 다르다면
+                    prefix = Array(prefix[..<i]) // prefix 갱신
                     break
                 }
             }
         }
-
         return prefix.isEmpty ? "" : prefix.joined()
     }
 }
