c9b8baea3a02e364b3241ca4197e35b05c2eda3e
Bambu <byrr113@gmail.com>
2026-01-19 11:59:48 +0900

    Update 나머지가 1이 되는 수 찾기.swift

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.swift
@@ -1,8 +1,22 @@
 import Foundation
 
+// 최종 풀이(다른 사람 풀이 참고)
 func solution(_ n:Int) -> Int {
     for x in 2...n {
         if n % x == 1 { return x }
     }
     return 0
+}
+
+// 기존 풀이
+func solution2(_ n: Int) -> Int {
+    var x = 0
+
+    for num in 2... {
+        if n % num == 1 {
+            x = num
+            break
+        }
+    }
+    return x
 }
