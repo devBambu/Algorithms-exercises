6036c1330ab01e3a2ac322437730d2e33ec1784a
Bambu <byrr113@gmail.com>
2026-01-21 12:18:42 +0900

    [Bronze V] Title: 사칙연산, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let input = readLine() ?? ""
+let arr = input.components(separatedBy: .whitespaces)
+let a = Int(arr[0])!
+let b = Int(arr[1])!
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
