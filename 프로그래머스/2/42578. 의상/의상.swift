b91fcbbb575c4078150cd9be7e0ca18e7066c23d
Bambu <byrr113@gmail.com>
2026-01-21 10:56:11 +0900

    Update 의상.swift

--- 프로그래머스/2/42578. 의상/의상.swift
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -1,22 +1,40 @@
 import Foundation
 
+// 튜터님과 다른 분들의 조언으로 바꾼 답안...
 func solution(_ clothes:[[String]]) -> Int {
     // 의상 종류 key
-    
     var dic: [String: Int] = [:]
     
     clothes.forEach {
         dic[$0[1], default: 0] += 1
     }
-    var arr = Array(dic.values).map { $0 + 1 }
+    let arr = Array(dic.values).map { $0 + 1 }
+
+    return arr.reduce(1, *) - 1
+}
+
+// 기존 답안
+// 근데 애초에 경우의 수를 구하는 식 자체가 이상했던 것 같음..
+func solution2(_ clothes: [[String]]) -> Int {
+    // 의상 종류 key
+    var dic: [String: Int] = [:]
+    
+    clothes.forEach {
+        dic[$0[1], default: 0] += 1
+    }
+
+    // value.count * value.count * ... -> 하나씩 없애가면서
+
+    var arr = Array(dic.values)
     var result = 0
     
-    return arr.reduce(1, *) - 1
-    
-//     while !arr.isEmpty {
-//         result += arr.reduce(1, *)
-//         arr.removeFirst()
-//     }
-    
-//     return result - 1
+    while !arr.isEmpty {
+        result += arr.reduce(1, *)
+        if arr.count != 1 {
+            result += arr.first! // 의상 단독으로만 입는 경우 추가
+        }
+        arr.removeFirst()
+    }
+        
+    return result
 }
