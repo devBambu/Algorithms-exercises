ea11788668606f2df4f12ff400e289bfddaeddec
Bambu <byrr113@gmail.com>
2026-01-21 17:40:56 +0900

    Update 개수 세기.swift

--- 백준/Bronze/10807. 개수 세기/개수 세기.swift
+++ 백준/Bronze/10807. 개수 세기/개수 세기.swift
@@ -10,4 +10,9 @@ var count = 0
 for n in arr {
     if n == num { count += 1 }
 }
+
 print(count)
+
+//8번 ~ 14번 라인 기존 풀이는 아래였는데, 백준에서 채점 시 컴파일 에러가 나길래 위와 같이 변경하였음.
+
+print(arr.count(where: { $0 == num })
