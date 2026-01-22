d17c94cfb79bfeb76f84c1f1c32ad587d040f8da
Bambu <byrr113@gmail.com>
2026-01-22 12:58:21 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ dot:[Int]) -> Int {
+    switch (dot[0], dot[1]) {
+        case (0..., 0...): return 1
+        case (...0, 0...): return 2
+        case (...0, ...0): return 3
+        case (0..., ...0): return 4
+        default: return 0
+    }
+}
