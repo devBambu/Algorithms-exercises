aa69d0eb689fe79270066c214addbbcb13583ab8
Bambu <byrr113@gmail.com>
2026-01-22 12:10:06 +0900

    Update 뒤집힌 문자열.swift

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.swift
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+// 최종 답안
 func solution(_ my_string:String) -> String {
     return String(my_string.reversed())
 }
+
+// 기존 코드
+func solution(_ my_string: String) -> String {
+    return my_string.map{ String($0) }.reversed().joined()
+}
+// 원래 return my_string.reversed() 로만 작성했으나 실패
+// -- my_string.reversed()의 타입은 ReversedCollection<String>이기 때문에!
+// -- 근데 그걸 몰라서 매핑해서 풀었었음..
