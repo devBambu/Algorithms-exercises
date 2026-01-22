5e7b04f2c56c1c9e7fb17cda7be099202fa166b7
Bambu <byrr113@gmail.com>
2026-01-22 10:42:38 +0900

    Update 대소문자 바꾸기.swift

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.swift
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.swift
@@ -1,3 +1,4 @@
+// 최종 제출 코드
 import Foundation
 
 let input = readLine() ?? ""
@@ -5,3 +6,13 @@ let input = readLine() ?? ""
 print(input.reduce(into: "") { 
     $0 += ($1.isUppercase ? $1.lowercased() : $1.uppercased())
 })
+
+
+// 기존 코드
+// - reduce를 활용해서 더 축약할 수 있을 것 같아 수정하였음
+var result = ""
+for c in input {
+    result = c.isUppercase ? result + c.lowercased() : result + c.uppercased()
+}
+
+print(result)
