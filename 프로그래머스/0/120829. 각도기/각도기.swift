bf7742e227d8065602ac46c39f8f2286c83535ad
Bambu <byrr113@gmail.com>
2026-01-22 11:55:49 +0900

    [level 0] Title: 각도기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    switch angle {
+        case ..<90: return 1
+        case 90: return 2
+        case ..<180: return 3
+        case 180: return 4
+        default: return 0
+    }
+}
