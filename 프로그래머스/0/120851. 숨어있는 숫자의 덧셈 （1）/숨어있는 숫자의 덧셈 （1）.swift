7fa8991541c784b5afbaed540565e8d7e7583545
Bambu <byrr113@gmail.com>
2026-01-27 11:03:42 +0900

    Update 숨어있는 숫자의 덧셈 （1）.swift

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
@@ -1,5 +1,14 @@
 import Foundation
 
+// 기존 답안
 func solution(_ my_string:String) -> Int {
     return my_string.compactMap{ Int(String($0)) }.reduce(0, +)
+}
+
+// 최종 답안
+// - 실행 속도가 (쪼끔) 더 빠른 느낌
+func solution2(_ my_string:String) -> Int {
+    return my_string.reduce(into: 0) {
+        $0 += Int(String($1)) ?? 0
+    }
 }
