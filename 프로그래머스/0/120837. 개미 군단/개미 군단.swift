17a86ba294d71c8ac2c42ebb639919e603daf146
Bambu <byrr113@gmail.com>
2026-01-27 15:02:03 +0900

    Update 개미 군단.swift

--- 프로그래머스/0/120837. 개미 군단/개미 군단.swift
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.swift
@@ -1,5 +1,31 @@
 import Foundation
 
+// 기존 답안
+// - while문 내에서 switch문을 돌려 정답을 구한다.
+func solution1(_ hp:Int) -> Int {
+    var ant = 0
+    var target = hp
+    
+    while target > 0 {
+        switch target {
+        case 5...:
+            ant += target / 5
+            target = target % 5
+        case 3..<5:
+            ant += target / 3
+            target = target % 3
+        case ..<3:
+            ant += target / 1
+            target = 0
+        default:
+            continue
+        }
+    }
+    return ant
+}
+
+// 최종 답안
+// 다른 사람의 풀이 참고 -- 기존 답안에서도 5로 1번 나누고, 3으로 1번 나누고, 1로 한번 나누는데, switch문으로 해야할 필요가 없다 생각하여 수정
 func solution(_ hp:Int) -> Int {
     var target = hp
  
