0f7eaa2355c3e7caf441a421a914475205870330
Bambu <byrr113@gmail.com>
2026-02-10 19:27:29 +0900

    Update 가장 큰 수.swift

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    // "610" > "106" 중 큰 수를 비교하여 큰 것대로 나열
+    // [6, 10, 2]일 때, "610" > "106" 중 큰 수를 비교해 큰 것대로 나열
     let result = numbers.sorted(by: { 
         ($0.description + $1.description) > ($1.description + $0.description) 
     }).map(String.init).joined()
     
-    // 결과가 0이라면 "0000" 형태로 되어있을 가능성 -- "0"으로 반환
+    // 주어진 numbers가 [0, 0, 0, 0]이라면 결과가 "0000" -> "0"으로 변환
     return Int(result) == 0 ? "0" : result
 }
