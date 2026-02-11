c171cf335de883fad5fd0e1002e776284c1bc88e
Bambu <byrr113@gmail.com>
2026-02-11 11:39:13 +0900

    Update 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.swift

--- 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.swift
+++ 프로그래머스/0/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.swift
@@ -8,3 +8,9 @@ func solution(_ myString:String, _ pat:String) -> String {
     
     return result
 }
+
+// 다른 사람의 풀이
+func solution(_ myString: String, _ pat: String) -> String {
+    guard let range = myString.range(of: pat, options: .backward) else { return "" } // of:가 있는 범위를 반환 - options: .backward는 뒤에서부터 찾음. 즉, myStirng의 뒤에서부터 of:가 있는 범위를 찾아 반환
+    return myString.prefix(upTo: range.upperBound)
+}
