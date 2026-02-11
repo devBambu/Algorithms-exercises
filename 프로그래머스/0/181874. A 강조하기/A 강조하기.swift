47c2e5a376b0a72ebb3d99e4070a5ade88deaeb3
Bambu <byrr113@gmail.com>
2026-02-11 11:03:25 +0900

    Update A 강조하기.swift

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.swift
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.swift
@@ -5,3 +5,8 @@ func solution(_ myString:String) -> String {
         $1 == "a" || $1 == "A" ? $0 + "A" : $0 + $1.lowercased()
     }
 }
+
+// 다른 사람의 풀이
+func solution(_ myString: String) -> String {
+    return myString.lowercased().replacintOccurence(of: "a", with: "A")
+}
