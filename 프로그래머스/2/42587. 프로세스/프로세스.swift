b2dde080a7cecda22c75a132620141a2be165bc1
Bambu <byrr113@gmail.com>
2026-02-12 12:13:34 +0900

    Update 프로세스.swift

--- 프로그래머스/2/42587. 프로세스/프로세스.swift
+++ 프로그래머스/2/42587. 프로세스/프로세스.swift
@@ -2,6 +2,27 @@ import Foundation
 
 func solution(_ priorities:[Int], _ location:Int) -> Int {
     var queue = priorities.enumerated().map { (location: $0, priority: $1) }
+    var count = 0
+    
+    while !queue.isEmpty {
+        let run = queue.removeFirst()
+        let maxPriority = queue.map { $0.priority }.max() ?? 0 // 프로세스 대기열 큐에서 가장 큰 우선순위
+        
+        if run.priority < maxPriority { // 큐에 우선순위 큰 게 남아있는지? -- contains가 아닌 비교 연산자로 비교하여 더 빠르게 연산 가능
+            // 있을 경우 - 큐에 재삽입
+            queue.append(run)
+        } else {
+            // 없을 경우 - while문 탈출
+            count += 1
+            if run.location == location { break }
+        }
+    }
+    return count
+}
+
+// count 변수가 없는 풀이
+func solution(_ priorities:[Int], _ location:Int) -> Int {
+    var queue = priorities.enumerated().map { (location: $0, priority: $1) }
     
     while !queue.isEmpty {
         let run = queue.removeFirst()
@@ -12,9 +33,8 @@ func solution(_ priorities:[Int], _ location:Int) -> Int {
             queue.append(run)
         } else {
             // 없을 경우 - while문 탈출
-            if run.location == location { 
-                // 기존 프로세스 수 - 대기중인 프로세스 수 = 현재 실행중인 프로세스 순서
-                return priorities.count - queue.count 
+            // 기존 프로세스 수 - 대기중인 프로세스 수 = 현재 꺼낸 프로세스의 실행 순서
+                return priorities.count - queue.count
             }
         }
     }
