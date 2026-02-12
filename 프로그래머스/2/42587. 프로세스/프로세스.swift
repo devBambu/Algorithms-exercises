1a78c4516a3399175b856fa8696c6bce396690a3
Bambu <byrr113@gmail.com>
2026-02-12 12:07:54 +0900

    [level 2] Title: 프로세스, Time: 7.25 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.swift
+++ 프로그래머스/2/42587. 프로세스/프로세스.swift
@@ -2,19 +2,21 @@ import Foundation
 
 func solution(_ priorities:[Int], _ location:Int) -> Int {
     var queue = priorities.enumerated().map { (location: $0, priority: $1) }
-    var count = 0
     
     while !queue.isEmpty {
         let run = queue.removeFirst()
-        let maxPriority = queue.map { $0.priority }.max() ?? 0
+        let maxPriority = queue.map { $0.priority }.max() ?? 0 // 프로세스 대기열 큐에서 가장 큰 우선순위
+        
         if run.priority < maxPriority { // 큐에 우선순위 큰 게 남아있는지?
             // 있을 경우 - 큐에 재삽입
             queue.append(run)
         } else {
-            // 없을 경우 - count 1 추가
-            count += 1
-            if run.location == location { break } // 탈출문 추가하면 while 다 안돌아도됨
+            // 없을 경우 - while문 탈출
+            if run.location == location { 
+                // 기존 프로세스 수 - 대기중인 프로세스 수 = 현재 실행중인 프로세스 순서
+                return priorities.count - queue.count 
+            }
         }
     }
-    return count
+    return 0
 }
