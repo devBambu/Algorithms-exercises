56dbd83f458d1ef90ffd339ce9f54025984e02d1
Bambu <byrr113@gmail.com>
2026-02-12 10:57:29 +0900

    Update 가장 큰 수.swift

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,9 +1,11 @@
 import Foundation
 
-func solution(_ numbers:[Int]) -> String {    
+func solution(_ numbers:[Int]) -> String {
+    // sorted 내에서 문자열로 변환하여 비교하지 말고 애초에 문자열로 변환해서 sorted에서 비교
     let result = numbers.map(String.init).sorted(by: {
         ($0 + $1) > ($1 + $0)
     })
-    
+
+    // prefix가 0을 포함하는지 확인
     return result.joined().hasPrefix("0") ? "0" : result.joined()
 }
