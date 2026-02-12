95ebaa62dc008421e2d2953855212fa603239a3b
Bambu <byrr113@gmail.com>
2026-02-12 12:21:29 +0900

    Update ad 제거하기.swift

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.swift
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.swift
@@ -5,3 +5,8 @@ func solution(_ strArr:[String]) -> [String] {
         if !$1.contains("ad") { $0.append($1) } 
     }
 }
+
+// 다른 사람의 풀이 - filter 쓰면 됐음..
+func solution(_ strArr: [String]) -> [String] {
+    return strArr.filter { !$0.contains("ad") }
+}
