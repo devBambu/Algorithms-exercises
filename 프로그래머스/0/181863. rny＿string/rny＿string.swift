c8c4767605c218f4933349076507cca84ae6e678
Bambu <byrr113@gmail.com>
2026-02-13 11:45:03 +0900

    Update rny＿string.swift

--- 프로그래머스/0/181863. rny＿string/rny＿string.swift
+++ 프로그래머스/0/181863. rny＿string/rny＿string.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ rny_string:String) -> String {
-    let ans = rny_string.replacingOccurrences(of: "m", with: "rn")
-    return ans
+    return rny_string.replacingOccurrences(of: "m", with: "rn")
 }
