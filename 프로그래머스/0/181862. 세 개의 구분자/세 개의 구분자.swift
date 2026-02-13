135a1a14d0ab2d472f1de497e581226eeceb098c
Bambu <byrr113@gmail.com>
2026-02-13 11:53:05 +0900

    Update 세 개의 구분자.swift

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.swift
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.swift
@@ -4,3 +4,10 @@ func solution(_ myStr:String) -> [String] {
     let result = myStr.components(separatedBy: ["a", "b", "c"]).filter { !$0.isEmpty }
     return result.isEmpty ? ["EMPTY"] : result
 }
+
+// 다른 사람의 풀이
+// - split(whereSeparator:) 사용 시 whereSeparator에 클로저 사용 가능
+func solution(_ myStr: String) -> [String] {
+    let result = myStr.split { $0 == "a" || $0 == "b" || $0 == "c" }.map(String.init)
+    return result.isEmpty ? ["EMPTY"] : result
+}
