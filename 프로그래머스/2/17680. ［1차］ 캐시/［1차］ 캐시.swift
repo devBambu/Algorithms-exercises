cd3272a4abc638674c2295c4c7bc398255a52ca3
Bambu <byrr113@gmail.com>
2026-02-19 14:40:28 +0900

    Update ［1차］ 캐시.swift

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.swift
@@ -1,3 +1,42 @@
+// 배열을 활용한 최종 답안
+func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
+    // LRU 캐시: 가장 오랫동안 사용되지 않은 캐시를 삭제
+    
+    // 캐시 사이즈가 0일 경우
+    guard cacheSize != 0 else { return cities.count * 5 }
+
+    // 캐시 사이즈가 0이 아닐 경우
+    var runTime = 0 // 실행 시간
+    
+    // 배열의 인덱스 == 캐시 실행 순서
+    // --> 실행 오래된 순 (0 > 1 > 2), 2가 가장 최근
+    var cacheCity = [String]() // 캐시 도시 이름
+    
+    for e in cities {
+        let city = e.lowercased()
+        
+        if cacheCity.contains(city) { // cache hit
+            // 실행 순서 변경
+            let i = cacheCity.firstIndex(of: city)!
+            cacheCity.append(cacheCity.remove(at: i))
+            
+            runTime += 1
+        } else { // cache miss
+            if cacheCity.count >= cacheSize { // 빈 캐시가 없을 경우
+                // 캐시 삭제
+                cacheCity.removeFirst()
+            }
+            
+            // 신규 캐시 추가
+            cacheCity.append(city)
+            runTime += 5
+        }
+    }
+    return runTime
+}
+
+// 문제 오독으로 인한 오답 - 캐시 삭제 기준을 (최저 실행 횟수 + 가장 오래된 캐시) 라고 이해함
+// 배열을 2개 사용 - 실행 횟수 / 도시 이름
 func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
     // LRU 캐시: 가장 오랫동안 사용되지 않은 캐시를 삭제
     
@@ -25,8 +64,12 @@ func solution(_ cacheSize:Int, _ cities:[String]) -> Int {
             runTime += 1
         } else { // cache miss
             if cacheCity.count >= cacheSize { // 빈 캐시가 없을 경우
+                let minCount = cacheCount.min()! // 캐시 중 가장 적은 실행 횟수
+                let i = cacheCount.firstIndex(of: minCount)! // minCount 중 가장 실행이 오래된 캐시
+                
                 // 캐시 삭제
-                cacheCity.removeFirst()
+                cacheCity.remove(at: i)
+                cacheCount.remove(at: i)
             }
             
             // 신규 캐시 추가
