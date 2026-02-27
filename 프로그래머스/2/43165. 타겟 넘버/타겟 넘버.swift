30f82e88ed2463376b391c1679e727b78d99933d
Bambu <byrr113@gmail.com>
2026-02-27 09:54:13 +0900

    [level 2] Title: 타겟 넘버, Time: 243.61 ms, Memory: 27.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -3,15 +3,13 @@ import Foundation
 func solution(_ numbers:[Int], _ target:Int) -> Int {
     var results = [0]
     for num in numbers {
-        let twoNum = [num, num * -1]
-        let temp = results.reduce(into: [Int]()) {
-            let a = $1 + twoNum[0]
-            let b = $1 + twoNum[1]
+        results = results.reduce(into: [Int]()) {
+            let a = $1 + num
+            let b = $1 - num
             
             $0.append(a)
             $0.append(b)
         }
-        results = temp
     }
     
     return results.filter { $0 == target }.count
