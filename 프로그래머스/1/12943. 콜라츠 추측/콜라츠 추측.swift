04e8e851e8e72bd8d3edeb76f49259aa3b648141
Bambu <byrr113@gmail.com>
2026-03-09 09:12:42 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
@@ -0,0 +1,21 @@
+func solution(_ num:Int) -> Int {
+    if num == 1 { return 0 }
+    
+    var count = 0
+    var result = num
+    
+    for attempt in 0..<500 {
+        if result.isMultiple(of: 2) {
+            result = result / 2
+        } else {
+            result = result * 3 + 1
+        }
+        
+        if result == 1 { 
+            count = attempt + 1
+            break 
+        }
+    }
+    
+    return result == 1 ? count : -1
+}
