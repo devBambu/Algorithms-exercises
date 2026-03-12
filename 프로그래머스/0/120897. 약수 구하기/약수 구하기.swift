ab2c1fdca463fc242c9e534ff0cb91dfa3e5b642
Bambu <byrr113@gmail.com>
2026-03-12 14:15:10 +0900

    [level 0] Title: 약수 구하기, Time: 5.79 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.swift
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-    return (0...n).reduce([Int]()) {
-        n.isMultiple(of: $1) ? $0 + [$1] : $0
-    }
+    return (1...n).filter { n.isMultiple(of: $0) }
 }
