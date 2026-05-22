8a9e30618eae95acbefb3cd19d981c1d8b7b44d2
Bambu <byrr113@gmail.com>
2026-05-22 10:42:13 +0900

    [level 0] Title: A로 B 만들기, Time: 3.28 ms, Memory: 15.9 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.swift
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.swift
@@ -1,19 +1,23 @@
 import Foundation
 
-func solution(_ before:String, _ after:String) -> Int {
-    let beforeDictionary = makeDictionary(of: before)
-    let afterDictionary = makeDictionary(of: after)
+// func solution(_ before:String, _ after:String) -> Int {
+//     let beforeDictionary = makeDictionary(of: before)
+//     let afterDictionary = makeDictionary(of: after)
     
-    return beforeDictionary == afterDictionary ? 1 : 0
-}
+//     return beforeDictionary == afterDictionary ? 1 : 0
+// }
 
-func makeDictionary(of text: String) -> [Character: Int] {
-    let array = text.map { $0 }
-    var dic: [Character: Int] = [:]
+// func makeDictionary(of text: String) -> [Character: Int] {
+//     let array = text.map { $0 }
+//     var dic: [Character: Int] = [:]
     
-    for letter in array {
-        dic[letter, default: 0] += 1
-    }
+//     for letter in array {
+//         dic[letter, default: 0] += 1
+//     }
     
-    return dic
+//     return dic
+// }
+
+func solution(_ before:String, _ after:String) -> Int {
+    return before.sorted() == after.sorted() ? 1 : 0
 }
