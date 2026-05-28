a8e794039d2cc2807ee111e3009218ca5a8f5763
Bambu <byrr113@gmail.com>
2026-05-28 11:52:37 +0900

    [level 0] Title: 가까운 수, Time: 0.28 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/README.md
+++ 프로그래머스/0/120890. 가까운 수/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가까운 수 - 120890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120890?language=swift) 
 
 ### 성능 요약
 
-메모리: 15.9 MB, 시간: 0.22 ms
+메모리: 15.8 MB, 시간: 0.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 28일 11:49:01
+2026년 05월 28일 11:52:35
 
 ### 문제 설명
 
--- 프로그래머스/0/120890. 가까운 수/가까운 수.swift
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.swift
@@ -2,5 +2,13 @@ import Foundation
 
 func solution(_ array:[Int], _ n:Int) -> Int {
     
-    return array.sorted(by: { $0 < $1 }).sorted(by: { abs($0 - n) < abs($1 - n) }).first!
+    return array.sorted(by: { 
+        if abs($0 - n) < abs($1 - n) {
+            return true
+        } else if abs($0 - n) == abs($1 - n) {
+            return $0 < $1
+        } else {
+            return false
+        }
+    }).first!
 }
