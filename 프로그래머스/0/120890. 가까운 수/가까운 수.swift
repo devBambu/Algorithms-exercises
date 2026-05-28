1977993b0f4d4dffdb7523c3deb9066cf569a883
Bambu <byrr113@gmail.com>
2026-05-28 11:55:31 +0900

    [level 0] Title: 가까운 수, Time: 0.73 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.swift
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.swift
@@ -2,13 +2,5 @@ import Foundation
 
 func solution(_ array:[Int], _ n:Int) -> Int {
     
-    return array.sorted(by: { 
-        if abs($0 - n) < abs($1 - n) {
-            return true
-        } else if abs($0 - n) == abs($1 - n) {
-            return $0 < $1
-        } else {
-            return false
-        }
-    }).first!
+    return array.min { (abs($0 - n), $0) < (abs($1 - n), $1) }!
 }
