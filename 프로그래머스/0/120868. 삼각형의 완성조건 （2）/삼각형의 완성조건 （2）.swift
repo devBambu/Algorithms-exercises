b6c0690b792e7396cd63633639470a2c24c1279a
Bambu <byrr113@gmail.com>
2026-07-16 20:23:13 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.25 ms, Memory: 20 MB -BaekjoonHub

---
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+func solution(_ sides:[Int]) -> Int {
+    let max = sides.max()! // 매개변수 중 최댓값
+    let min = sides.min()!
+    var results: Set<Int> = []
+    
+    // max가 가장 긴 변일 경우
+    for x in (max - min)...max {
+        if max < min + x {
+            results.insert(x)
+        }
+    }
+    
+    // x가 가장 긴 변일 경우
+    for x in (max + 1)..<(max + min) {
+        
+        results.insert(x)
+    }
+
+    return results.count
+}
