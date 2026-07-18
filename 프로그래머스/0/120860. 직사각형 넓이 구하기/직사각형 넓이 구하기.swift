9a9363efd227d6f1b07db2887098bbd12486f916
Bambu <byrr113@gmail.com>
2026-07-18 23:12:24 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.02 ms, Memory: 20.9 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.swift
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.swift
@@ -1,16 +1,8 @@
 import Foundation
 
 func solution(_ dots:[[Int]]) -> Int { 
-    var width = 0
-    var height = 0
+    let xs = dots.map { $0[0] }
+    let ys = dots.map { $0[1] }
     
-    for dot in dots {
-        if dot[0] == dots[0][0] {
-            height = abs(dots[0][1] - dot[1])
-        } else if dot[1] == dots[0][1] {
-            width = abs(dots[0][0] - dot[0])
-        }
-    }
-    
-    return width * height
+    return (xs.max()! - xs.min()!) * (ys.max()! - ys.min()!)
 }
