a7af4ae5a37ba31ee2792f305daecc7b71ab50c9
Bambu <byrr113@gmail.com>
2026-07-19 17:06:55 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.05 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/README.md
+++ 프로그래머스/0/120871. 저주의 숫자 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 저주의 숫자 3 - 120871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120871?language=swift) 
 
 ### 성능 요약
 
-메모리: 20.2 MB, 시간: 0.07 ms
+메모리: 20.7 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 19일 17:05:40
+2026년 07월 19일 17:06:52
 
 ### 문제 설명
 
--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.swift
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.swift
@@ -1,27 +1,13 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    // 15 -> 25
-    // 5(3의 배수 개수) -> 20
-    // 15 ~ 20 = 1개 -> 21
-    // 21은 3의 배수 = 1개 -> 22
-    // 22까지 '3'포함 = 1개 (13) -> 23
-    // 23은 '3'포함 = 1개 -> 24
-    // 24는 3의 배수 -> 25
-
     var num = 0
     
     for target in 1...n {
         num += 1
         
-        while true {
-            if num % 3 == 0 { // 3의 배수일 경우
-                num += 1
-            } else if String(num).contains("3") { // 3이 포함된 경우
-                num += 1
-            } else {
-                break
-            }
+        while num % 3 == 0 || String(num).contains("3") {
+            num += 1
         }
     }
     
