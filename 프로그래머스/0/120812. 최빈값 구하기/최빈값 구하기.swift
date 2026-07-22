3768b04b3bc9ad09b5a6c3c68b2675f2a6ea0077
Bambu <byrr113@gmail.com>
2026-07-22 17:44:42 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.14 ms, Memory: 20.4 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.swift
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.swift
@@ -1,12 +1,9 @@
 import Foundation
 
 func solution(_ array:[Int]) -> Int {
-    let dic = array.reduce(into: [Int: Int]()) {
-        $0[$1, default: 0] += 1
-    }
-    
-    let max = dic.values.max()!
-    let filter = dic.filter { $0.value == max }
+    let dic = Dictionary(grouping: array) { $0 }.sorted { $0.value.count > $1.value.count }
+    let max = dic.first!.value.count
+    let filter = dic.filter { $0.value.count == max }
     
     return filter.count == 1 ? filter.first!.key : -1
 }
