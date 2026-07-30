20721840cd67afc59e14f3a4423ae2602bd95c97
Bambu <byrr113@gmail.com>
2026-07-30 17:17:53 +0900

    [level 1] Title: 노란불 신호등, Time: 1412.68 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.swift
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.swift
@@ -7,24 +7,21 @@ func solution(_ signals:[[Int]]) -> Int {
     // G G Y R R: 2초, 1초, 2초
     // (5 - 1) % 5 = 4: 인덱스
     
-    let colors = signals.reduce(into: [[String]]()) { colors, signal in
-        let green = Array(repeating: "G", count: signal[0])
-        let yellow = Array(repeating: "Y", count: signal[1])
-        let red = Array(repeating: "R", count: signal[2])
-                                                     
-        colors.append(green + yellow + red)
-    }
-    
-    let counts = colors.map { $0.count }
-    let lcm = counts.reduce(1) { currentLcm, num in 
+    let cycles = signals.map { $0.reduce(0, +) }
+    let lcm = cycles.reduce(1) { currentLcm, num in 
             currentLcm / gcd([currentLcm, num]) * num
                                } // 반복문 종료 범위
     
     for second in 1...lcm {
-        if colors.reduce(into: [String]()) { current, color in
-            let index = (second - 1) % color.count
-            current.append(color[index])
-        }.allSatisfy { $0 == "Y"} {
+        if cycles.enumerated().reduce(into: [Bool]()) { isYellow, cycle in
+            let index = (second - 1) % cycle.element
+            let yellowRange = signals[cycle.offset][0]..<(signals[cycle.offset][0] + signals[cycle.offset][1])
+                                                                            
+            if yellowRange.contains(index) {
+                isYellow.append(true)
+            } else {
+                isYellow.append(false)
+            }                                                                        }.allSatisfy { $0 == true } {
             return second
         }
     }
