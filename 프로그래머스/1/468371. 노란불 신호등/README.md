8f21c8758385fa69d804da161c4ea3a0bc3de6ae
Bambu <byrr113@gmail.com>
2026-07-30 17:02:27 +0900

    [level 1] Title: 노란불 신호등, Time: 536.83 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/README.md
+++ 프로그래머스/1/468371. 노란불 신호등/README.md
@@ -1,10 +1,10 @@
 # [level 1] 노란불 신호등 - 468371 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/468371) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/468371?language=swift) 
 
 ### 성능 요약
 
-메모리: 20.6 MB, 시간: 659.81 ms
+메모리: 20.8 MB, 시간: 536.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 30일 16:48:27
+2026년 07월 30일 17:02:25
 
 ### 문제 설명
 
--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.swift
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.swift
@@ -4,10 +4,8 @@ func solution(_ signals:[[Int]]) -> Int {
     // 초 -> 노 -> 빨
     // 신호등이 모두 노랑일 때 정전
     
-    // 5 - 2, 3 % 3 == 0, 13 - 2 = 11 , 11 % 1 == 0, 12 - 2 = 10, 10 % 2 == 0
-    
-    // G G Y R R
-    // (5 - 1) % 5 = 4
+    // G G Y R R: 2초, 1초, 2초
+    // (5 - 1) % 5 = 4: 인덱스
     
     let colors = signals.reduce(into: [[String]]()) { colors, signal in
         let green = Array(repeating: "G", count: signal[0])
@@ -18,8 +16,9 @@ func solution(_ signals:[[Int]]) -> Int {
     }
     
     let counts = colors.map { $0.count }
-    let gcd = gcd(counts)
-    let lcm = counts.map { $0 / gcd }.reduce(gcd, *)
+    let lcm = counts.reduce(1) { currentLcm, num in 
+            currentLcm / gcd([currentLcm, num]) * num
+                               } // 반복문 종료 범위
     
     for second in 1...lcm {
         if colors.reduce(into: [String]()) { current, color in
