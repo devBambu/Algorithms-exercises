1aa95a80d23d96c3e6308889f40e0ef2d4fbe332
Bambu <byrr113@gmail.com>
2026-08-09 16:34:22 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.07 ms, Memory: 20.1 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가운데 글자 가져오기 - 12903 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12903?language=swift) 
 
 ### 성능 요약
 
-메모리: 20.4 MB, 시간: 0.11 ms
+메모리: 20.1 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 09일 16:12:57
+2026년 08월 09일 16:34:19
 
 ### 문제 설명
 
--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
@@ -1,12 +1,11 @@
 func solution(_ s:String) -> String {
     guard s.count > 1 else { return s } // s의 길이가 1일 때 빠른 종료
-    
-    let characters = s.map { String($0) }
+
     let i = s.count / 2
-    let indices = s.count % 2 == 0 ? [i - 1, i] : [i]
     
-    return characters.enumerated()
-    .filter { indices.contains($0.offset) }
-    .map { $0.element }
-    .joined()
+    if s.count % 2 == 0 {
+        return String(Array(s)[i - 1...i])
+    } else {
+        return String(Array(s)[i])
+    }
 }
