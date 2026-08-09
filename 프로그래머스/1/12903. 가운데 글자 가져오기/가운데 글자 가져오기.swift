e481ffe36135b1369df300277f8a632e3c52b17a
Bambu <byrr113@gmail.com>
2026-08-09 16:10:26 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 20.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.swift
@@ -0,0 +1,11 @@
+func solution(_ s:String) -> String {
+    guard s.count > 1 else { return s } // s의 길이가 1일 때 빠른 종료
+    
+    let characters = s.map { String($0) }
+    let i = s.count / 2
+    let indices = s.count % 2 == 0 ? [i - 1, i] : [i]
+    
+    return indices.reduce("") { 
+        $0 + characters[$1]
+    }
+}
