1162a2239b1dc093b021ef50271b7d3770c0f9c5
Bambu <byrr113@gmail.com>
2026-08-11 17:12:25 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/README.md
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 부족한 금액 계산하기 - 82612 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/82612) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/82612?language=swift) 
 
 ### 성능 요약
 
-메모리: 20.6 MB, 시간: 0.50 ms
+메모리: 20.7 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 11일 16:52:11
+2026년 08월 11일 17:12:22
 
 ### 문제 설명
 
--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.swift
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.swift
@@ -1,9 +1,14 @@
 import Foundation
 
-func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{
-    let balance = (1...count).reduce(money) {
-        $0 - ($1 * price)
-    }
+func solution(_ price:Int, _ money:Int, _ count:Int) -> Int64{    
+    // n(a + I) / 2 --- 등차수열의 합: a는 첫째 항, I는 마지막 항, n은 count 
+    
+    // amount = count(price + price * count) / 2
+    // (price * count + price * count * count ) / 2
+    // ((price * count)(1 + count)) / 2
+    
+    let amount = ((price * count) * (1 + count)) / 2
+    let balance = money - amount
     
     return balance > 0 ? 0 : Int64(balance * -1)
 }
