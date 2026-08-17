ca10f8de5597d2da4a5b374949d82d2c7f1ed9d5
Bambu <byrr113@gmail.com>
2026-08-17 18:31:45 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 20.6 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/README.md
+++ 프로그래머스/1/131705. 삼총사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 삼총사 - 131705 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131705?language=swift) 
 
 ### 성능 요약
 
-메모리: 20.6 MB, 시간: 0.01 ms
+메모리: 20.6 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 17일 17:53:59
+2026년 08월 17일 18:31:42
 
 ### 문제 설명
 
--- 프로그래머스/1/131705. 삼총사/삼총사.swift
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -1,15 +1,21 @@
 import Foundation
 
+// dfs 재귀함수를 활용한 풀이
 func solution(_ number:[Int]) -> Int {
     var result = 0
     
-    for i in 0..<number.count - 2 {
-        for j in (i + 1)..<number.count - 1 {            
-            for k in (j + 1)..<number.count {
-                result = number[i] + number[j] + number[k] == 0 ? result + 1 : result
-            }
+    func dfs(_ start: Int, _ sum: Int, _ count: Int) {
+        if count == 3 { // 고른 숫자가 3개일 때 재귀 호출 종료
+            result = sum == 0 ? result + 1 : result
+            return
+        }
+        
+        for i in start..<number.count { // start부터 시작함으로써 중복 원소 선택 방지
+            dfs(i + 1, sum + number[i], count + 1)
         }
     }
+    
+    dfs(0, 0, 0)
     
     return result
 }
