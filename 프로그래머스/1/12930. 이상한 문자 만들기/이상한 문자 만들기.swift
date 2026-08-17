674e9ad42b23363c6cddc880dd21134996343da8
Bambu <byrr113@gmail.com>
2026-08-17 20:07:49 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.30 ms, Memory: 20.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.swift
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.swift
@@ -1,10 +1,9 @@
 func solution(_ s:String) -> String {
     return s.components(separatedBy: .whitespaces).enumerated()
-    .reduce("") { string, word in
-            let transformed = word.element.enumerated().reduce("") {
+    .map { word in 
+        word.element.enumerated().reduce("") {
                 $1.offset % 2 == 0 ? $0 + $1.element.uppercased() : $0 + $1.element.lowercased()
             }
-   
-            return word.offset == 0 ? string + transformed : string + " " + transformed
     }
+    .joined(separator: " ")
 }
