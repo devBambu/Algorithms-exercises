efc5ac8cd9e90aa104bd8bcda1b4a2fcb0e2893d
Bambu <byrr113@gmail.com>
2026-08-18 11:13:35 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.swift
@@ -1,19 +1,6 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    var num = n
-    var triple = [Int]()
-    
-    while num > 0 {
-        triple.append(num % 3)
-        num /= 3
-    }
-    
-    return triple.enumerated().reduce(0) { result, target in
-        let count = (triple.count - 1) - target.offset
-        let decimal = (0..<count).reduce(target.element) { decimal, _ in
-            decimal * 3
-        }
-        return result + decimal
-    }
+    let triple = String(n, radix: 3)
+    return Int(String(triple.reversed()), radix: 3)!
 }
