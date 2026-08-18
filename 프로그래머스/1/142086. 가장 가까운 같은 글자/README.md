97a47839a03fb146424c03df9a2c16a8346d2481
Bambu <byrr113@gmail.com>
2026-08-18 12:01:34 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 974.82 ms, Memory: 22 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 가까운 같은 글자 - 142086 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142086?language=swift) 
 
 ### 성능 요약
 
-메모리: 21.5 MB, 시간: 20.61 ms
+메모리: 22 MB, 시간: 974.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 18일 11:53:14
+2026년 08월 18일 12:01:31
 
 ### 문제 설명
 
--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
@@ -1,14 +1,8 @@
 import Foundation
 
 func solution(_ s:String) -> [Int] {
-    let characters = s.map { String($0) }
-    
-    return characters.enumerated().map {
-        if characters.firstIndex(of: $0.element)! == $0.offset {
-            return -1
-        } else {
-            let last = characters[0..<$0.offset].lastIndex(of: $0.element)!
-            return $0.offset - last
-        }
+    return s.enumerated().map {
+        let last = Array(s)[0..<$0.offset].lastIndex(of: $0.element)
+        return last == nil ? -1 : $0.offset - last!
     }
 }
