83fe0de25dee828d49011493a879aa1ca81c51ce
Bambu <byrr113@gmail.com>
2026-08-18 12:06:37 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 12.31 ms, Memory: 21.3 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.swift
@@ -4,7 +4,10 @@ func solution(_ s:String) -> [Int] {
     let arr = Array(s)
     
     return s.enumerated().map {
-        let last = arr[0..<$0.offset].lastIndex(of: $0.element)
-        return $0.offset - (last ?? ($0.offset + 1))
+        if let last = arr[0..<$0.offset].lastIndex(of: $0.element) {
+            return $0.offset - last
+        } else {
+            return -1
+        }
     }
 }
