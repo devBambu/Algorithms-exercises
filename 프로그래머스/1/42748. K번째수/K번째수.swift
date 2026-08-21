b99ed9289c929983dcf6d1f16b83935293a75b74
Bambu <byrr113@gmail.com>
2026-08-21 10:33:30 +0900

    [level 1] Title: K번째수, Time: 0.18 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -1,13 +1,9 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    var result = [Int]()
-    
-    for command in commands {
+    return commands.map { command in
         let (i, j, k) = (command[0] - 1, command[1], command[2] - 1)
         let slice = array[i..<j].sorted()
-        result.append(slice[k])
+        return slice[k]
     }
-    
-    return result
 }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=swift) 
 
 ### 성능 요약
 
-메모리: 20.5 MB, 시간: 0.05 ms
+메모리: 20.3 MB, 시간: 0.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 21일 10:31:58
+2026년 08월 21일 10:33:27
 
 ### 문제 설명
 
