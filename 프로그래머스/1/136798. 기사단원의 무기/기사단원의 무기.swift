4ad030b8742a684b879ca170c467e19f282fe9f2
Bambu <byrr113@gmail.com>
2026-09-04 13:43:08 +0900

    [level 1] Title: 기사단원의 무기, Time: 110.14 ms, Memory: 20.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.swift
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.swift
@@ -4,16 +4,14 @@ func solution(_ number:Int, _ limit:Int, _ power:Int) -> Int {
     let atks = (1...number).map { knight in
         guard knight > 1 else { return 1 }
                                  
-        var atk = 0
+        var atk = 0  
+        var n = 1
                                  
-        for n in (1..<knight) {
-            if n * n > knight { break }
-            if n * n == knight {
-                atk += 1
-                break
-            } else if knight % n == 0 {
-                atk += 2 // 약수는 쌍을 이루기 때문에 +2
-            }
+        while n * n <= knight {
+            atk = n * n == knight ? atk + 1
+            : knight % n == 0 ? atk + 2 : atk // 약수는 쌍을 이루기 때문에 +2
+            
+            n += 1
         }
                                  
         return atk > limit ? power : atk
