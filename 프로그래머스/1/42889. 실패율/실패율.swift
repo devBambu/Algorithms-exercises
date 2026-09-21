dcede4f73dc4bf51febeb51681ec7711fa8c0432
Bambu <byrr113@gmail.com>
2026-09-21 17:55:00 +0900

    [level 1] Title: 실패율, Time: 21.72 ms, Memory: 29.6 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.swift
+++ 프로그래머스/1/42889. 실패율/실패율.swift
@@ -6,14 +6,24 @@ func solution(_ N:Int, _ stages:[Int]) -> [Int] {
     // stages: 플레이어들의 현재 스테이지 번호
     // return: 실패율이 높은 스테이지부터 내림차순 배열
     
+    var stagePlayers = [Int: Int]() // [스테이지: 현재 스테이지에 머물고 있는 플레이어 수]
+    
+    for s in stages {
+        stagePlayers[s, default: 0] += 1
+    }
+    
+    var players = stages.count // 스테이지 도달 플레이어 수
+    
     return (1...N).map { stage in
-        let players = stages.count(where: { $0 >= stage }) // 스테이지 도달 플레이어 수
-        let failed = stages.count(where: { $0 == stage }) // 미클리어 플레이어 수
+        let failed = stagePlayers[stage] ?? 0 // 미클리어 플레이어 수
         
         if players == 0 || failed == 0 {
             return Double(0)
         } else {
-            return Double(failed) / Double(players)
+            let failureRate = Double(failed) / Double(players)
+            players -= failed // 미클리어 플레이어 수를 다음 스테이지 도달 플레이어 수에서 제외
+            
+            return failureRate
         }
     }.enumerated()
     .sorted {
