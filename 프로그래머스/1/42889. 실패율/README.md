4a7170cd98a9a03a9b4e372764bbac6e848a5f6a
Bambu <byrr113@gmail.com>
2026-09-21 17:43:31 +0900

    [level 1] Title: 실패율, Time: 6601.24 ms, Memory: 29.3 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/README.md
+++ 프로그래머스/1/42889. 실패율/README.md
@@ -1,10 +1,10 @@
 # [level 1] 실패율 - 42889 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42889?language=swift) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 4962.41 ms
+메모리: 29.3 MB, 시간: 6601.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 21일 17:26:50
+2026년 09월 21일 17:43:29
 
 ### 문제 설명
 
--- 프로그래머스/1/42889. 실패율/실패율.swift
+++ 프로그래머스/1/42889. 실패율/실패율.swift
@@ -6,15 +6,9 @@ func solution(_ N:Int, _ stages:[Int]) -> [Int] {
     // stages: 플레이어들의 현재 스테이지 번호
     // return: 실패율이 높은 스테이지부터 내림차순 배열
     
-    var stagePlayers = [Int: Int]() // [스테이지: 현재 스테이지에 머물고 있는 플레이어 수]
-    
-    for s in stages {
-        stagePlayers[s, default: 0] += 1
-    }
-    
     return (1...N).map { stage in
-        let players = stages.filter { $0 >= stage }.count // 스테이지 도달 플레이어 수
-        let failed = stagePlayers[stage] ?? 0
+        let players = stages.count(where: { $0 >= stage }) // 스테이지 도달 플레이어 수
+        let failed = stages.count(where: { $0 == stage }) // 미클리어 플레이어 수
         
         if players == 0 || failed == 0 {
             return Double(0)
