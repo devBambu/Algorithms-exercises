ff4f91832e05cf19ca8a4c7f76a99c3a7aefc68d
Bambu <byrr113@gmail.com>
2026-09-22 15:44:17 +0900

    [level 1] Title: 둘만의 암호, Time: 0.09 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652?language=swift) 
 
 ### 성능 요약
 
-메모리: 20.4 MB, 시간: 0.08 ms
+메모리: 20 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 09월 22일 15:41:38
+2026년 09월 22일 15:44:15
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.swift
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.swift
@@ -1,15 +1,12 @@
 import Foundation
 
 func solution(_ s:String, _ skip:String, _ index:Int) -> String {
-    let alphabet = [
-        "a", "b", "c", "d", "e", "f", "g",
-        "h", "i", "j", "k", "l", "m", "n", "o", "p", 
-        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-        ]
-    let skipped = alphabet.filter { !skip.contains($0) }
+    let skipped = "abcdefghijklmnopqrstuvwxyz"
+    .filter { !skip.contains($0) }
+    .map { $0 }
     
     return s.reduce("") { result, target in
-        let i = (skipped.firstIndex(of: String(target))! + index) % skipped.count
-        return result + skipped[i]
+        let i = (skipped.firstIndex(of: target)! + index) % skipped.count
+        return result + String(skipped[i])
     }
 }
